54c4cb1f8a086eb1253ee4fe180280b3da7477be
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-15 15:47:10 -0400

    progress on parseFile

--- src/com/coderscampus/assignment4/StudentService.java
+++ src/com/coderscampus/assignment4/StudentService.java
@@ -1,14 +1,58 @@
 package com.coderscampus.assignment4;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
-public abstract class StudentService {
+public class StudentService {
 
     public static void parseFile(String filePath) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+
+        // setup local variables
+        final String DESTINATION_PATH = "src/com/coderscampus/assignment4";
+        final String FILE_NAME_1 = DESTINATION_PATH + "course1.csv";
+        final String FILE_NAME_2 = DESTINATION_PATH + "course2.csv";
+        final String FILE_NAME_3 = DESTINATION_PATH + "course3.csv";
+        final String COURSE_NAME_1 = "COMPSCI";
+        final String COURSE_NAME_2 = "APMTH";
+        final String COURSE_NAME_3 = "STAT";
+        final int STUDENT_ARRAY_SIZE = 100;
+        Student[] course1Students = new Student[STUDENT_ARRAY_SIZE];
+        Student[] course2Students = new Student[STUDENT_ARRAY_SIZE];
+        Student[] course3Students = new Student[STUDENT_ARRAY_SIZE];
+
+        // initialize readers and writers in try with resources block
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
+             BufferedWriter writer1 = new BufferedWriter(new FileWriter(FILE_NAME_1));
+             BufferedWriter writer2 = new BufferedWriter(new FileWriter(FILE_NAME_2));
+             BufferedWriter writer3 = new BufferedWriter(new FileWriter(FILE_NAME_3));) {
+
+            // write file header to each file
+            final String fileHeader = "Student ID,Student Name,Course,Grade\n";
+            writer1.write(fileHeader);
+            writer2.write(fileHeader);
+            writer3.write(fileHeader);
+
+            // read through csv file and parse into appropriate Student arrays
+            String currentLine;
+            int count = 0;
+            while ((currentLine = reader.readLine()) != null) {
+                if (currentLine.contains(COURSE_NAME_1)) {
+                    String[] splitLine = currentLine.split(",");
+                    course1Students[count] = new Student(splitLine[0], splitLine[1], splitLine[2], splitLine[3]);
+                } else if (currentLine.contains(COURSE_NAME_2)) {
+                    String[] splitLine = currentLine.split(",");
+                    course2Students[count] = new Student(splitLine[0], splitLine[1], splitLine[2], splitLine[3]);
+                } else if (currentLine.contains(COURSE_NAME_3)) {
+                    String[] splitLine = currentLine.split(",");
+                    course3Students[count] = new Student(splitLine[0], splitLine[1], splitLine[2], splitLine[3]);
+                } else {
+                    String invalidCourseName = currentLine.split(",")[2].split(" ")[0];
+                    System.err.println("No such course name: " + invalidCourseName);
+                }
+                ++count;
+            }
+
+
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + filePath);
@@ -18,4 +62,5 @@ public abstract class StudentService {
             System.out.println(e.getMessage());
         }
     }
+
 }
