1a12e700156789fd01adb2297b91ccdac8abd7da
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-15 14:23:51 -0400

    method stub parseFile() in StudentService

--- src/com/coderscampus/assignment4/StudentService.java
+++ src/com/coderscampus/assignment4/StudentService.java
@@ -1,4 +1,21 @@
 package com.coderscampus.assignment4;
 
-public class StudentService {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public abstract class StudentService {
+
+    public static void parseFile(String filePath) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filePath);
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("File read failed: " + filePath);
+            System.out.println(e.getMessage());
+        }
+    }
 }
